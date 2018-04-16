81f7fdf1825c6d6c042278bb3a2b459582b3a363
Shuo Li <ls90911@gmail.com>
2018-04-16 20:46:40 +0200

    aa

--- real_gate_test_rfsnake.m
+++ real_gate_test_rfsnake.m
@@ -14,7 +14,7 @@ FIGURE = 0;
 %load('2018_2_5_GT_with_SG');
 
 n = 1;
-m = 1;
+m = 20;
 
 TP_rate_mean = zeros(m-n+1,1);
 FP_rate_mean = zeros(m-n+1,1);
@@ -29,54 +29,54 @@ max_iter = 10;
 for i = n:m
     i
 
-    %minimun_length = (i-1)*5;
-    minimun_length = 25;
-
-    color_fitness_threshold = (i-1)*0.05;
+%     minimun_length = (i-1)*5;
+%     %minimun_length = 25;
+% 
+% %     color_fitness_threshold = (i-1)*0.05;
 %     color_fitness_threshold = 0.7;
-    sample_num = 1000;
-    p = 1;
-    while p <= max_iter
-        [detected_gate,gates_candidate_corners] = snake_gate_detection(dir_name,0,1000);
-        %
-        %  load('2018_3_8_raw_detection');
-        % %
+%     sample_num = 1000;
+%     p = 1;
+%     while p <= max_iter
+%         [detected_gate,gates_candidate_corners] = snake_gate_detection(dir_name,0,1000);
 %         [refined_gate_candidates,refined_gate_candidates_cf] = refine_gate_candidates(gates_candidate_corners);
 %         [TP,TN,FP,FN] = count_ROC_term_with_refined_candidates(GT_gate,refined_gate_candidates,...
 %             refined_gate_candidates_cf,gates_candidate_corners);
-        [TP,TN,FP,FN] = count_ROC_with_onboard_detection_algorithm(detected_gate,GT_gate);
-        ROC_statistic{i}(p,1) = TP;
-        ROC_statistic{i}(p,2) = TN;
-        ROC_statistic{i}(p,3) = FP;
-        ROC_statistic{i}(p,4) = FN;
-        ROC_statistic{i}(p,5) = TP/(TP + FN);
-        ROC_statistic{i}(p,6) = FP/(FP + TN);
-        ROC_statistic{i}(p,7) = FP/ size(GT_gate,1) ;
-        p = p+1;
-    end
-    TP_rate_mean(i) = mean(ROC_statistic{i}(:,5));
-    FP_rate_mean(i) = mean(ROC_statistic{i}(:,6));
-    TP_rate_std(i) = std(ROC_statistic{i}(:,5));
-    FP_rate_std(i) = std(ROC_statistic{i}(:,6));
-    FP_per_imag_mean(i) = mean(ROC_statistic{i}(:,7));
-    FP_per_imag_std(i) = std(ROC_statistic{i}(:,7));
+% %         [TP,TN,FP,FN] = count_ROC_with_onboard_detection_algorithm(detected_gate,GT_gate);
+%         ROC_statistic{i}(p,1) = TP;
+%         ROC_statistic{i}(p,2) = TN;
+%         ROC_statistic{i}(p,3) = FP;
+%         ROC_statistic{i}(p,4) = FN;
+%         ROC_statistic{i}(p,5) = TP/(TP + FN);
+%         ROC_statistic{i}(p,6) = FP/(FP + TN);
+%         ROC_statistic{i}(p,7) = FP/ size(GT_gate,1) ;
+%         p = p+1;
+%     end
+%     TP_rate_mean(i) = mean(ROC_statistic{i}(:,5));
+%     FP_rate_mean(i) = mean(ROC_statistic{i}(:,6));
+%     TP_rate_std(i) = std(ROC_statistic{i}(:,5));
+%     FP_rate_std(i) = std(ROC_statistic{i}(:,6));
+%     FP_per_imag_mean(i) = mean(ROC_statistic{i}(:,7));
+%     FP_per_imag_std(i) = std(ROC_statistic{i}(:,7));
 end
 
-load('3_24_desktop');
-% load('3_24_laptop');
+%  load('3_24_desktop');
+ load('3_24_laptop');
 
 figure(10)
 hAx=axes;
- hAx.XScale='log';
-xlim([10^-2 20]);
+%  hAx.XScale='log';
+% xlim([10^-2 20]);
 hold all
-% errorbar(FP_rate_mean,TP_rate_mean,...
-%     TP_rate_std,TP_rate_std,FP_rate_std,FP_rate_std,'o')
+
 errorbar(FP_per_imag_mean,TP_rate_mean,...
     TP_rate_std,TP_rate_std,FP_per_imag_std,FP_per_imag_std,'o')
 
+fig.real_race = plot(0.07,0.4476,'o','MarkerSize',10);
 grid on
-
+legend(fig.real_race,'Algorithm used in drone race 2017')
+x = [0.19 0.34];
+y = [0.486 0.43];
+annotation('textarrow',x,y,'String','y = x ')
 xlabel('# FPs/image');
 ylabel('True Positive rate');
 temp = 1;
